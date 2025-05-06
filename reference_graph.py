dfdfb813317f5d1d01ef4329ade8d661e9aea556
DBanksStudent <77126758+DBanksStudent@users.noreply.github.com>
2025-05-06 06:17:03 -0700

    basic graph visualization with networkx
    I am amazed at how much of a pain this was. The ReferenceGraph object can now visualize itself. There is a test method in the main_gui to validate this.

--- reference_graph.py
+++ reference_graph.py
@@ -1,5 +1,10 @@
 from node import Node
 
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
 class ReferenceGraph:
     def __init__(self):
         self.nodes = {}
@@ -19,3 +24,27 @@ class ReferenceGraph:
     def display_graph(self): # Display the graph in a readable format
         for node in self.nodes.values():
             print(f"{node.document_name} -> {[neighbor.document_name for neighbor in node.get_neighbors()]}")
+
+
+
+    ## Basically networkx has it's own graph datastructure
+    ## we turn the ReferenceGraph object into a nx.Graph object and it will plot itself.
+    def visualize_graph(self, **options): 
+        netx_graph = nx.DiGraph()
+        netx_graph.add_nodes_from(self.nodes)
+        print(list(netx_graph.nodes))
+        
+        for node in self.nodes.values(): # for each node in the graph
+            for neighbor_node in node.get_neighbors(): ## iterate thru list of neighbors ## we have a list of keys now.
+                #print("neighbor:",neighbor)
+                #print("self.nodes:",self.nodes)
+                #neighbor_node = self.nodes[neighbor]
+                #new_edge = tuple(node.document_name,neighbor_node.document_name,{'weight': node.edges[neighbor_node]}) ### of form, start end, weight (2, 3, {'weight': 3.1415})
+                #print(node.edges[neighbor_node])
+                netx_graph.add_edge(node.document_name,neighbor_node.document_name,weight=node.edges[neighbor_node])
+                print("found valid edge")
+        
+        print(netx_graph)
+        pos = nx.random_layout(netx_graph)
+        nx.draw(netx_graph,pos,None,**options) ##tookout with_labels=True since it's true by default
+        plt.show()
